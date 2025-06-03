571e87af2dbf85f684391321a1c885e4e5e83d78
addix999 <addixvideo@gmail.com>
2025-06-03 21:17:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def chatbot(message):
     return "Balasan: " + message
 
 iface = gr.Interface(fn=chatbot, inputs="text", outputs="text", title="Chatbot Gradio Simple")
-iface.launch(server_name="0.0.0.0", server_port=8080), share=True)
+iface.launch(server_name="0.0.0.0", server_port=8080, share=True)
